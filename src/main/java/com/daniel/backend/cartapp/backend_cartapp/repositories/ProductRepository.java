e596fef9c8c21d06a63154256a2f36ff1da7ff23
danifer24 <danielvf24@gmail.com>
2025-10-22 15:13:29 +0200

    Agregar estructura inicial del proyecto Spring Boot con configuración de Maven y entidades básicas

---
+++ src/main/java/com/daniel/backend/cartapp/backend_cartapp/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.daniel.backend.cartapp.backend_cartapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.daniel.backend.cartapp.backend_cartapp.models.entities.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+}
