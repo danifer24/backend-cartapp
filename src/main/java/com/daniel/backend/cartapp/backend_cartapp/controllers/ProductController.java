2fe84f8b0b6b69ea401da3791f2b4a705ff28053
danifer24 <danielvf24@gmail.com>
2025-10-30 14:31:23 +0100

    Agregar CrossOrigin para poder consumir la api desde el frontend

--- src/main/java/com/daniel/backend/cartapp/backend_cartapp/controllers/ProductController.java
+++ src/main/java/com/daniel/backend/cartapp/backend_cartapp/controllers/ProductController.java
@@ -7,10 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.daniel.backend.cartapp.backend_cartapp.services.ProductService;
 import com.daniel.backend.cartapp.backend_cartapp.models.entities.Product;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
     @Autowired
